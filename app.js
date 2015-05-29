197221aa7b818e4583cb09c0c333db7e3f0b15e4
sgreensmith <simongreensmith.office@gmail.com>
2015-05-29 17:22:50 +0100

    completed tutorial

--- app.js
+++ app.js
@@ -5,23 +5,38 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//\\ new code to enable access to mongodb using "monk"
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/node-express-mongo');
+
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express();
+var app = express();  //\\ this instantiates express and assigns "app" to it
 
-// view engine setup
+// view engine setup - //\\ tells app where to find views, and which engine to use to render them
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));  //\\ tells app to make all 'public' resources appear from http://localhost:3000/[folder]
 
+//\\ this makes our db accessible to the route (and needs to be "above" the following app.use routing
+//\\ this always adds a database object to every request that the client sends... which is not very efficient!
+app.use(function(req,res,next) {
+  req.db = db;
+  next();
+});
+
+//\\ this is the directive describing the routes to be used  and "establishing middleware for express"
 app.use('/', routes);
 app.use('/users', users);
 
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,46 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//\\ GET hello world page as per tutorial
+router.get('/helloworld', function(req, res, next) {
+  res.render('helloworld', {title: 'Hello, World!'})
+});
+
+//\\ new route that makes use of the database
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({},{},function(e, docs) {
+      res.render('userlist', {"userlist": docs });
+  });
+});
+
+//\\ new route for adding a user to the databse
+router.get('/newuser', function(req, res) {
+  res.render('newuser', {title: 'Add New User'});
+});
+
+//\\ the following enables app to deal with a POST to add user, pointed at /adduser
+router.post('/adduser', function(req,res) {
+
+    var db = req.db;    // set the database variable
+
+    var userName = req.body.username;       // extract data from request
+    var userEmail = req.body.useremail;     // extract data from request
+
+    var collection = db.get('usercollection');      // set the collection
+
+    collection.insert(
+        {"username" : userName, "useremail" : userEmail },
+        function (err, doc) {
+            if (err) {
+                res.send("There was a problem adding info to the database")
+            } else {
+                res.location("userlist");   //sets the client to userlist
+                res.redirect("userlist");   // redirects to userlist
+            }
+        }
+    );
+});
+
 module.exports = router;
